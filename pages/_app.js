d7bf1d7688acbce7280847581ec11e5c5d128bc4
asgoshawk <x5553434@gmail.com>
2022-06-27 15:02:23 +0800

    Fix the script in the head.

--- pages/_app.js
+++ pages/_app.js
@@ -41,15 +41,19 @@ const MyApp = ({ Component, pageProps }) => {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           strategy='afterInteractive'
         />
-        <script id='google-analytics' strategy='afterInteractive'>
-          {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+        <script 
+        id='google-analytics' 
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-                    `}
-        </script>
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                `
+        }}
+        />
       </Head>
       <Layout>
         {pageLoading ? (
